fe837409c410f26b18d7487e55a4db0dc35e4ccc
rstankov <rstankov@gmail.com>
2009-02-14 18:31:21 +0200

    Added Element.extractId

---
+++ src/extentions/element.js
@@ -0,0 +1,7 @@
+Element.addMethods({
+	// extracts id number from element(using it's id)
+	// exmaple: $('test_12).extractId() -> 12
+	extractId:  function(element){
+		return (element.id && element.id.match(/\w+_(\d+)/)[1]) || 0;
+	}
+});
