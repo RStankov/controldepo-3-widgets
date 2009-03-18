4ad562175aa02928f85e062dc16274031fdad3f4
rstankov <rstankov@gmail.com>
2009-03-18 21:42:05 +0200

    fix in element.extractId() example code

--- src/extentions/element.js
+++ src/extentions/element.js
@@ -1,6 +1,6 @@
 Element.addMethods({
 	// extracts id number from element(using it's id)
-	// exmaple: $('test_12).extractId() -> 12
+	// exmaple: $('test_12').extractId() -> 12
 	extractId:  function(element){
 		return (element.id && element.id.match(/\w+_(\d+)/)[1]) || 0;
 	}
