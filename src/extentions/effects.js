002b238eb8584b16fc570708de199c0145cd417d
rstankov <radoslavstankov@rstankov.local>
2009-04-08 19:08:06 +0300

    move initial morphing to beforeStartInternal callback

--- src/extentions/effects.js
+++ src/extentions/effects.js
@@ -1,29 +1,30 @@
 Effect.Mutate = function(from, into){
 	from = $(from);
-	from.absolutize();
-	from.makeClipping();
 	from.insert({before: $(into) || into});
 	
 	into = from.previous();
 	
 	var style = 'width: ' + into.getWidth() + 'px; height: ' + into.getHeight() + 'px;';
-	
-	into.setStyle({ width: from.getWidth() + 'px', height: from.getHeight() + 'px' });
-	into.makeClipping();
-	into.setOpacity(0.0);
-	into.show();
-	
 	return new Effect.Parallel([
 		new Effect.Morph(from, {sync: true, style: style + 'opacity: 0.0;'}),
 		new Effect.Morph(into, {sync: true, style: style + 'opacity: 1.0;'})
 	], Object.extend({
+		beforeStartInternal: function(){
+			from.absolutize();
+			from.makeClipping();
+			
+			into.setStyle({ width: from.getWidth() + 'px', height: from.getHeight() + 'px' });
+			into.makeClipping();
+			into.setOpacity(0.0);
+			into.show();
+		},
 		afterFinishInternal: function(e){
 			into.undoClipping();
 			into.style.width = null;
 			into.style.height = null;
 			into = null;
 			
-			if (e.options.replace){
+			if (e.options.repace){
 				from.remove();
 				from = null;
 				return;
