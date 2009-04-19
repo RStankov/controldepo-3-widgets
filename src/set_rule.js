35a59b501c47bda50a1738c6f976245efd122511
rstankov <radoslavstankov@rstankov.local>
2009-04-19 15:53:00 +0300

    added CD3.setRule, for setting style properties to CSS classes

---
+++ src/set_rule.js
@@ -0,0 +1,23 @@
+CD3.setRule = function(selector, styles){
+	var i, x, sheet, rules;
+
+	for (x = document.styleSheets.length - 1; 0 <= x ; x--){
+		sheet = document.styleSheets[x];
+		rules = sheet.cssRules || sheet.rules;
+		
+		for (i = rules.length - 1; 0 <= i; i--){
+			if (rules[i].selectorText == selector){
+				return Object.extend(rules[i].style, styles);
+			}
+		}
+	}
+	
+	var index = rules.length;
+	if (sheet.insertRule){ // Normal browsers
+		sheet.insertRule(selector + '{ }', index);
+	} else /* if (sheet.addRule) */ { // IE
+		sheet.addRule(selector, ';', index);
+	}
+	
+	Object.extend((sheet.cssRules || sheet.rules)[index].style, styles);
+}
