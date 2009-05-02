2eedab4f7242c4ffb003d99c1df99533834cd920
rstankov <radoslavstankov@rstankov.local>
2009-05-02 03:07:02 +0300

    added CD3.Behaviors.ScrollTo

---
+++ src/behaviors/scroll_to.js
@@ -0,0 +1,12 @@
+//= require <src/header.js>
+//= require <src/behaviors.js>
+// Usage: 
+//  CD3.Behaviors({
+//   'a[href=^#]': CD3.Behaviors.ScrollTo
+//  });
+CD3.Behaviors.ScrollTo = {
+	click: function(){
+		var href = this.getAttribute('href');
+		Effect.ScrollTo(href.substr(href.indexOf('#') + 1), { duration: 0.5 });
+	}
+}
