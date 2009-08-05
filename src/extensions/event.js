45c150c5e529cbface173cb8a9abe4f292c1732a
RStankov <rstankov@gmail.com>
2009-08-05 11:57:26 +0300

    some elements(document) do not have .ancestors method

--- src/extensions/event.js
+++ src/extensions/event.js
@@ -1,6 +1,6 @@
 (function(){
   function delegateHandler(e){
-    var element = e.element(), elements = [element].concat(element.ancestors());
+    var element = e.element(), elements = element.ancestors ? element.ancestors().concat([element]) : [element];
     ((Element.retrieve(this, 'prototype_delegates') || $H()).get(e.eventName || e.type) || []).each(function(pair){
       if (element = Selector.matchElements(elements, pair.key)[0])
         pair.value.invoke('call', element, e); 
