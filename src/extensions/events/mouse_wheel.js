1e3518b99cb13c5aef3af6580ff0dfe6b388cc79
RStankov <rstankov@gmail.com>
2009-08-23 00:16:26 +0300

    added Andrew Dupont mouse:wheel custom event

---
+++ src/extensions/events/mouse_wheel.js
@@ -0,0 +1,20 @@
+// credits to Andrew Dupont ( http://andrewdupont.net/2007/11/07/pseudo-custom-events-in-prototype-16/ )
+(function() {
+	function wheel(event){
+		var realDelta;
+
+		// normalize the delta
+		if (event.wheelDelta) // IE & Opera
+			realDelta = event.wheelDelta / 120;
+		else if (event.detail) // W3C
+			realDelta = -event.detail / 3;
+
+		if (!realDelta) return;
+
+		var customEvent = event.element().fire("mouse:wheel", { delta: realDelta });
+		if (customEvent.stopped) event.stop();
+	}
+
+	document.observe("mousewheel",     wheel);
+	document.observe("DOMMouseScroll", wheel);
+})();
