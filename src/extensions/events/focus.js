7ac3a450401082f9a7ca62093e8aa5b3b3544894
RStankov <rstankov@gmail.com>
2009-08-23 00:06:30 +0300

    added focus:in / focus:out cusom events

---
+++ src/extensions/events/focus.js
@@ -0,0 +1,16 @@
+(function() {
+	function focusInHandler(e){
+		e.element().fire("focus:in");
+	}
+	function focusOutHandler(e){
+		e.element().fire("focus:out");
+	}
+
+	if (document.addEventListener){
+		document.addEventListener("focus", focusInHandler, true);
+		document.addEventListener("blur", focusOutHandler, true);
+	} else {
+		document.observe("focusin", focusInHandler);
+		document.observe("focusout", focusOutHandler);
+	}
+})();
