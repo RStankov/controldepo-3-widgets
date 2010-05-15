1fa10aba1c16d0777b739ab703135c9d5111de93
RStankov <rstankov@gmail.com>
2010-05-15 15:58:22 +0300

    trace click (from labels) on the custom checkboxes

--- src/form/checkbox.js
+++ src/form/checkbox.js
@@ -1,22 +1,27 @@
 CD3.Form.Checkbox = Class.create({
-	initialize: function(checkbox){
-		this.checkbox	= $(checkbox).hide();
-		this.button		= new Element('a', {className:'checkbox', href:'javascript:;'}).update(' ');
-		
-		this.checkbox.insert({before: this.button});
-		this.button.observe('click', this.toggle.bind(this));
-		
-		if (this.checkbox.className.length > 0){
-			this.button.addClassName(this.checkbox.className);
-		}
-		
-		if (this.checkbox.checked){
-			this.button.addClassName('selected');
-		}
-	},
-	toggle: function(){
-		this.checkbox.checked = !this.checkbox.checked;
-		this.button[this.checkbox.checked ? 'addClassName' : 'removeClassName']('selected');
-		this.checkbox.fire('checkbox:clicked', { chechbox: this.checkbox });
-	}
+  initialize: function(element){
+    this.element	= $(element).hide();
+    this.button		= new Element('a', {className:'checkbox', href:'#'}).update(' ');
+
+    this.element.insert({before: this.button});
+    
+    this.element.observe('change', this.clicked.bind(this));
+    this.button.observe('click', this.toggle.bind(this));
+
+    if (this.element.className.length > 0){
+      this.button.addClassName(this.element.className);
+    }
+
+    if (this.element.checked){
+      this.button.addClassName('selected');
+    }
+  },
+  toggle: function(){
+    this.element.checked = !this.element.checked;
+    this.clicked();
+  },
+  clicked: function(){
+    this.button[this.element.checked ? 'addClassName' : 'removeClassName']('selected');
+    this.element.fire('checkbox:clicked', { chechbox: this.element });
+  }
 });
