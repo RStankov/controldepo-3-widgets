1a436c1e6c3db9cc8166402956fec9be2dbc29b7
RStankov <rstankov@gmail.com>
2010-05-15 13:49:33 +0300

    rewrite the scroller widget

--- src/widgets.js
+++ src/widgets.js
@@ -1,5 +1,6 @@
 CD3.Widget = {};
 
+//= require "widgets/base"
 //= require "widgets/accordion"
 //= require "widgets/drop_down"
 //= require "widgets/font_switcher"
--- src/widgets/scroller.js
+++ src/widgets/scroller.js
@@ -1,108 +1,104 @@
-CD3.Widget.Scroller = Class.create({
-	initialize: function(container, scroller, options){
-		// options
-		options = Object.extend({
-			styleArrow:		'arrow',
-			styleMoveUp:	'moveup',
-			styleMoveDown:	'movedown',
-			styleSlider:	'slider',
-			drag:           true,
-			speed:			1
-		}, options || {});					
-		
-		// base elements
-		this.speed		= options.speed;
-		this.container	= $(container);
-		this.scroller	= $(scroller);
-		this.handle		= this.scroller.down('.' + options.styleSlider);
-		
-		// arrows
-		this.scroller.select('.' + options.styleArrow).each(function(stop, arrow){
-			arrow.observe('mouseup',  stop);
-			arrow.observe('mouseout', stop);
-		}.curry(this.stopScroll.bind(this)));
-		
-		this.scroller.observe('mousedown', function(e){
-			var arrow = e.findElement('.' + options.styleArrow);
-			if (arrow) this.startScroll(arrow.hasClassName(options.styleMoveUp) ? -1 : 1);
-		}.bind(this));
-		
-		// handle
-		if (options.drag) new Draggable(this.handle,{ 
-			constraint:	'vertical', 
-			snap:		function(x, y){ return [x, this.validateTopPosition(y)]; }.bind(this),
-			change:		this.traceHandlePosition.bind(this)
-		});
+CD3.Widget.Scroller = Class.create(CD3.Widget.Base, {
+  setup: function(){
+    this.speed		= this.options.speed;
+    this.scroller	= $(this.options.scroller);
+    this.handle		= this.scroller.down('.' + this.options.styleSlider);
+    
+    /*
+    // TODO DRAG
+    if (options.drag) new Draggable(this.handle,{ 
+      constraint:	'vertical', 
+      snap:		function(x, y){ return [x, this.validateTopPosition(y)]; }.bind(this),
+      change:		this.traceHandlePosition.bind(this)
+    });
+    */
+    
+    var trackpath = this.handle.up();
 
-		// trackpath
-		var trackpath = $(this.handle.parentNode).observe('click', this.traceSliderClick.bind(this));
-		
-		this.trackpathPositionY	= trackpath.cumulativeOffset()[1];
-		this.sliderMaxHeight	= trackpath.getHeight() - this.handle.getHeight();
-					
-		// wheel
-		this.container.observe('mouse:wheel', this.traceMouseWheel.bind(this));
-		
-		// check if needed	
-		this.checkIfneeded();
-	},
-	startScroll: function(value){
-		this.interval = setInterval(this.scrollBy.bind(this, value), 3);
-	},
-	stopScroll: function(){
-		clearInterval(this.interval);
-		this.interval = null;
-	},
-	scrollBy: function(dir){
-		this.handle.style.top = this.validateTopPosition( this.getScrollPosition() + dir * this.speed ) + 'px';
-		this.traceHandlePosition();
-	},
-	setHandlePosition: function(){
-		this.handle.style.top = (this.sliderMaxHeight * (this.container.scrollTop / this.getVisibleHeight())) + 'px';
-	},
-	validateTopPosition: function(y){
-		if (y <= 0)						return 0;
-		if (y >= this.sliderMaxHeight)	return this.sliderMaxHeight;
-		
-		return y;
-	},
-	traceHandlePosition: function(){
-		this.container.scrollTop = this.getVisibleHeight() * (this.getScrollPosition() / this.sliderMaxHeight);
-	},
-	traceMouseWheel: function(e){
-		this.scrollBy(e.memo.delta > 0 ? -15 : 15);			
-	},
-	traceSliderClick: function(e){
-		var clickedY = e.pointerY()  - this.trackpathPositionY,
-			top		 = this.getScrollPosition(),
-			height	 = this.handle.getHeight();
-
-		// check to see we are no clicking the handle
-		if (clickedY < top || (top+height) < clickedY)
-			new Effect.Morph(this.handle, {
-				style:		 	{ top : this.validateTopPosition(clickedY) + 'px'},
-				duration:	 	0.5,
-				afterUpdate:	this.traceHandlePosition.bind(this),
-				queue:			{scope: 'cd3:scroller', limit:1}
-			});
-	},
-	getScrollPosition: function(){
-		return parseInt(this.handle.style.top) || 0;
-	},
-	getVisibleHeight: function(){
-		return this.container.scrollHeight - this.container.offsetHeight;
-	},
-	checkIfneeded: function(){
-		this.scroller[this.container.scrollHeight > this.container.offsetHeight ? 'hide' : 'show']();
-	}
-});
-CD3.Widget.Scroller.createIfNeeded = function(container, scroller, options){
-	container  = $(container);
-
-	if (container.scrollHeight <= container.offsetHeight){
-		$(scroller).hide();
-		return null;
-	}
+    this.trackpathPositionY = trackpath.cumulativeOffset()[1];
+    this.sliderMaxHeight    = trackpath.getHeight() - this.handle.getHeight();
 	
-	return new this(container, scroller, options);
+    this.checkIfneeded();
+  },
+  addObservers: function(){
+    var stop  = this.stopScroll.bind(this),
+        start = this.startScroll.bind(this),
+        match = '.' + this.options.styleArrow,
+        upArr = this.options.styleMoveUp;
+
+    this.observers = [
+      this.scroller.on('mouseup',    match, stop),
+      this.scroller.on('mouseout',   match, stop),
+      this.scroller.on('mousedown',  match, function(e, element){ start(element.hasClassName(upArr) ? -1 : 1); }),
+      this.handle.up().on('click',   this.traceSliderClick.bind(this)),
+      this.element.on('mouse:wheel', this.traceMouseWheel.bind(this))
+    ];
+  },
+  startScroll: function(value){
+    this.interval = setInterval(this.scrollBy.bind(this, value), 3);
+  },
+  stopScroll: function(){
+    clearInterval(this.interval);
+    this.interval = null;
+  },
+  scrollBy: function(dir){
+    this.handle.style.top = this.validateTopPosition( this.getScrollPosition() + dir * this.speed ) + 'px';
+    this.traceHandlePosition();
+  },
+  setHandlePosition: function(){
+    this.handle.style.top = (this.sliderMaxHeight * (this.element.scrollTop / this.getVisibleHeight())) + 'px';
+  },
+  validateTopPosition: function(y){
+    return y.constrain(0, this.sliderMaxHeight);
+  },
+  traceHandlePosition: function(){
+    this.element.scrollTop = this.getVisibleHeight() * (this.getScrollPosition() / this.sliderMaxHeight);
+  },
+  traceMouseWheel: function(e){
+    e.stop();
+    this.scrollBy(e.memo.delta > 0 ? -15 : 15);			
+  },
+  traceSliderClick: function(e){
+    var clickedY = e.pointerY()  - this.trackpathPositionY,
+        top      = this.getScrollPosition(),
+        height   = this.handle.getHeight();
+        
+    if (clickedY < top || ( top+height ) < clickedY){
+      new S2.FX.Morph(this.handle, {
+        style:        {top: this.validateTopPosition(clickedY) + 'px'},
+        duration:     0.5,
+        afterUpdate:  this.traceHandlePosition.bind(this)
+      }).play();
+    }
+  },
+  getScrollPosition: function(){
+    return parseInt(this.handle.style.top) || 0;
+  },
+  getVisibleHeight: function(){
+    return this.element.scrollHeight - this.element.offsetHeight;
+  },
+  checkIfneeded: function(){
+    this.scroller[this.element.scrollHeight > this.element.getHeight() ? 'show' : 'hide']();
+  }
+});
+
+CD3.Widget.Scroller.DEFAULT_OPTIONS = {
+  scroller:       null,
+  styleArrow:     'arrow',
+	styleMoveUp:    'moveup',
+	styleMoveDown:  'movedown',
+	styleSlider:    'slider',
+	drag:           true,
+	speed:          1
+};
+
+CD3.Widget.Scroller.createIfNeeded = function(container, scroller, options){
+  container  = $(container);
+
+  if (container.scrollHeight <= container.offsetHeight){
+    $(scroller).hide();
+    return null;
+  }
+	
+  return new this(container, scroller, options);
 }
