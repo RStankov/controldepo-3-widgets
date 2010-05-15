70588f980ccc164246a3567c41a6e2dcd05f7237
RStankov <rstankov@gmail.com>
2010-05-15 15:34:55 +0300

    rebuild CD3.Form.Select

--- src/form/select.js
+++ src/form/select.js
@@ -1,97 +1,102 @@
 CD3.Form.Select = Class.create(CD3.Widget.DropDown, {
-	initialize: function(select){		
-		select = $(select);
-		
-		// create replace elements
-		this.container	= new Element('span', {className:'dropper'});
-		this.link		= new Element('a', {href: 'javascript:;', className: 'drop'})
-		this.linkspan	= new Element('span').update(select.selectedIndex > -1 ? select.options[select.selectedIndex || 0].text : '');
-		this.hidden		= new Element('input', {type: 'hidden', name: select.name, value: select.getValue()});
-		this.div		= new Element('div').hide();
-		this.ul			= new Element('ul').observe('click', this.selectOption.bind(this));
-		
-		if (select.className)
-			this.container.addClassName(select.className);
-		
-		// insert container and all elements in it
-		select.insert({
-			 before: this.container
-				.insert(this.link.insert(this.linkspan))
-				.insert(this.hidden)
-				.insert(this.div.insert(this.ul))
-		});
-		
-		// get options
-		var options = Object.extend({
-			onChange: null,
-			topBottom: false,
-			reference: false,
-			scrollLimit: 6
-		}, arguments[1] || {});
+  NAME: 'CD3.Form.Select',
+  setup: function(){
+    var options = this.options;
+        select  = this.element;
+    
+    this.container  = new Element('span', {className: options.classNames.container});
+    this.trigger    = new Element('a', {href: '#', className: options.classNames.trigger})
+    this.span       = new Element('span');
+    this.menu		    = new Element('div').hide();
+    this.ul			    = new Element('ul');
+    this.element    = new Element('input', {type: 'hidden', name: select.readAttribute('select'), value: select.getValue()});
 
-		// add top / bottom lines		
-		if (options.topBottom)
-			this.ul.insert({
-				before:	new Element('span', {className: 'top'}).insert(new Element('span')),
-				after:	new Element('span', {className: 'bottom'}).insert(new Element('span'))
-			});
-			
-		// do we have on change event
-		if (options.onChange) this.onChange = options.onChange;
-		
-		// add options
-		$A(select.options).each(this.addOption.bind(this))
-		
-		// check for scrolled class
-		if (options.length > options.scrollLimit)
-			this.div.addClassName('scrolled');
-		
-		if (options.reference)
-			this.constructor.instances[Object.isString(options.reference) ? options.reference : select.name] = this;
-		
-		// delete old select
-		Element.remove(select);
-		
-		// add events
-		this.bindEvents();
-	},
-	destroy: function(){
-		this.unbindEvents();
-		this.removeOptions();
-		this.container.remove();
-	},
-	addOption: function (option){
-		this.ul.insert(new Element('li').insert(
-			new Element('a', {href: 'javascript:;'})
-				.store('option', {text: option.text, value: option.value != null ? option.value : option.text})
-				.update(option.text)
-		));
-	},
-	removeOptions: function(){
-		this.ul.select('li').each(function(li){
-			li.down('a').store('option', null).stopObserving('click');
-			li.remove();
-		});
-	},
-	setOptions: function(options, dontClear){
-		if (dontClear !== true) this.removeOptions();
+    if (select.className){
+      this.container.addClassName(select.className);
+    }
 
-		$A(options).each(this.addOption.bind(this));
+    select.insert({
+      before: this.container.insert([
+        this.element,
+        this.trigger.insert(this.span),
+        this.menu.insert(this.ul)
+      ])
+    });
 
-		if (dontClear !== true) this.select(options[0]);
-	},
-	selectOption: function(e){
-		var element = e.findElement('a');
-		if (element) this.select(element.retrieve('option'));
-	},
-	select: function(option){
-		this.linkspan.innerHTML = option.text;
-		this.hidden.value		= option.value;
-		this.hide();
-		
-		if (this.onChange)
-			this.onChange.call(this, option.value);
-	}
+    if (options.topBottom){
+      this.ul.insert({
+        before:	new Element('span', {className: options.classNames.top}).insert(new Element('span')),
+        after:	new Element('span', {className: options.classNames.bottom}).insert(new Element('span'))
+      });
+    }
+
+    if (options.onChange){
+      this.onChange = options.onChange;
+    }
+
+    this.set(select.options);
+    
+    if (options.length > options.scrollLimit){
+      this.div.addClassName(options.classNames.scrolled);
+    }
+
+    if (options.reference){
+      this.constructor.instances[Object.isString(options.reference) ? options.reference : select.name] = this;
+    }
+
+    Element.remove(select);
+    delete options;
+  },
+  addObservers: function($super){
+    $super();
+    this.observers.push(this.menu.on('click', 'a', this.selectOption.bind(this)));
+  },
+  destroy: function(){
+    this.container.purge().remove();
+    this.removeObservers();
+  },
+  add: function(option){
+    this.ul.insert(
+      new Element('li').insert(
+        new Element('a', {href: '#'})
+          .store('option', {text: option.text, value: option.value != null ? option.value : option.text})
+          .update(option.text)
+    ));
+  },
+  set: function(options, dontClear){
+    if (dontClear !== true) this.ul.select('li').each(function(li){ li.purge().remove(); });
+
+    $A(options).each(this.add.bind(this));
+
+    if (dontClear !== true) this.select(options[0]);
+  },
+  selectOption: function(e, element){
+    this.select(element.retrieve('option'));
+  },
+  select: function(option){
+    var oldValue = this.element.getValue();
+    
+    this.span.update(option.text);
+    this.element.setValue(option.value);
+    this.hide();
+
+    if (this.onChange && oldValue != option.value){
+      this.onChange.call(this, option.value);
+    }
+  }
 });
 
+CD3.Form.Select.DEFAULT_OPTIONS = {
+  classNames:   {
+    trigger:    'drop',
+    container:  'dropper',
+    scrolled:   'scrolled',
+    top:        'top',
+    bottom:     'bottom'
+  },
+  onChange:     null,
+	topBottom:    false,
+	reference:    false,
+	scrollLimit:  6
+}
 CD3.Form.Select.instances = {};
--- src/widgets/drop_down.js
+++ src/widgets/drop_down.js
@@ -7,10 +7,10 @@ CD3.Widget.DropDown = CD3.Widget.create('CD3.Widget.DropDown', {
 	  this.menu    = this.element.down(this.options.menu).hide();
 	},
 	addObservers: function(){
-	  this.documentObserver = new Event.Handler(document, 'click', null, this.close.bind(this));
+	  this.documentObserver = new Event.Handler(document, 'click', null, this.hide.bind(this));
 	  this.documentObserver.start = this.documentObserver.start.bind(this.documentObserver)
 	  this.observers = [
-	    this.open.on('click', this.toggle.bind(this)),
+	    this.trigger.on('click', this.toggle.bind(this)),
 	    this.documentObserver
 	  ];
 	},
@@ -18,14 +18,15 @@ CD3.Widget.DropDown = CD3.Widget.create('CD3.Widget.DropDown', {
 		this[this.menu.visible() ? 'hide' : 'show']();
 	},
 	show: function(){
-		this.menu.show();
-		this.documentObserver.start.defer();
+	  if (!this.menu.visible()){
+		  this.menu.show();
+		  this.documentObserver.start.defer();
+	  }
 	},
 	hide: function(){
-		this.menu.hide();
-		this.documentObserver.stop();
-	},
-	close: function(){
-		if (this.menu.visible()) this.hide();
+	  if (this.menu.visible()){
+		  this.menu.hide();
+		  this.documentObserver.stop();
+	  }
 	}
 });
