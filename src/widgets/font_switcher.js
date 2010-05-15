c38db458a49ff468700852ac26080982673b9025
RStankov <rstankov@gmail.com>
2010-05-15 14:05:44 +0300

    CD3.Widget.FontSwitcher now uses CD3.WidgetBase

--- src/widgets/font_switcher.js
+++ src/widgets/font_switcher.js
@@ -1,48 +1,52 @@
-CD3.Widget.FontSwitcher = Class.create({
-	initialize: function(panel, content, options){
-		panel	= $(panel);
-		options = Object.extend({
-			className:	'text-size-',
-			max: 		4,
-			reset:		'.reset',
-			plus:		'.plus',
-			minus:		'.minus',
-			callback:	false
-		}, options || {});
-		
-		if (options.plus)	panel.delegate(options.plus,  'click', this.change.bind(this,  1));
-		if (options.reset)	panel.delegate(options.reset, 'click', this.change.bind(this,  0));
-		if (options.minus)	panel.delegate(options.minus, 'click', this.change.bind(this, -1));
-		
-		this.size		= 0;
-		this.maxSize	= options.max;
-		this.className	= options.className;
-		this.content	= $(content);
-		this.callback	= options.callback;
-		
-		this.content.select('font[size]').each(function(font){
-			font._size = parseInt(font.getAttribute('size'));
-		});
-	},
-	change: function(size, e){
-		if (e && 'stop' in e) e.stop();
-		
-		size = size == 0 ? 0 : this.size + size, 0;
-		size = size <  0 ? 0 : (size > this.maxSize ? this.maxSize : size);
-		
-		if (this.size != 0){
-			this.content.removeClassName(this.className + this.size);
-		}
-			
-		if (size != 0){
-			this.content.addClassName(this.className + size);
-		}
-		
-		this.size = size;
-		this.content.select('font[size]').each(function(font){
-			font.setAttribute('size', font._size + size);
-		});
-		
-		if (this.callback) this.callback.call(this);
-	}
+CD3.Widget.FontSwitcher = Class.create(CD3.Widget.Base, {
+  NAME: "CD3.Widget.FontSwitcher",
+  
+  setup: function(){
+    this.content    = $(this.options.content);
+    this.size       = this.element.getAttribute('data-size') || 0;
+
+    this.content.select('font[size]').each(function(font){
+      font.store('original:size', parseInt(font.getAttribute('size')));
+    });
+  },
+  addObservers: function(){
+    var options = this.options,
+        change  = this.change;
+    
+    if (options.plus)  this.observers.push(this.element.on('click', options.plus,  change.bind(this,  1)));
+    if (options.reset) this.observers.push(this.element.on('click', options.reset, change.bind(this,  0)));
+    if (options.minus) this.observers.push(this.element.on('click', options.minus, change.bind(this, -1)));
+  },
+  change: function(size, e){
+    if (e && Object.isFunction(e.stop)) e.stop();
+
+    size = (size == 0 ? 0 : this.size + size).constrain(0, this.options.max);
+
+    var options = this.options;
+
+    if (this.size != 0){
+      this.content.removeClassName(options.className + this.size);
+    }
+
+    if (size != 0){
+      this.content.addClassName(options.className + size);
+    }
+
+    this.size = size;
+    this.content.select('font[size]').each(function(font){
+      font.setAttribute('size', font.retrieve('original:size') + size);
+    });
+
+    if (Object.isFunction(options.callback)) options.callback.call(this, this);
+  }
 });
+
+CD3.Widget.FontSwitcher.DEFAULT_OPTIONS = {
+  className:	'text-size-',
+  max: 		    4,
+  reset:		  '.reset',
+  plus:		    '.plus',
+  minus:		  '.minus',
+  callback:   false,
+  content:    null
+};
